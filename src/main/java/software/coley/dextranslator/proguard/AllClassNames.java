dbdce5a8e747f8763d086237e9030785714fd570
Col-E <mcoley2@gmu.edu>
2023-04-18 03:29:30 -0400

    Add PG filter to match any name

---
+++ src/main/java/software/coley/dextranslator/proguard/AllClassNames.java
@@ -0,0 +1,67 @@
+package software.coley.dextranslator.proguard;
+
+import com.android.tools.r8.graph.DexType;
+import com.android.tools.r8.shaking.ProguardClassNameList;
+import com.android.tools.r8.shaking.ProguardTypeMatcher;
+import com.android.tools.r8.utils.TraversalContinuation;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+/**
+ * Proguard name filter matching all class names.
+ *
+ * @author Matt Coley
+ */
+public class AllClassNames extends ProguardClassNameList {
+	/**
+	 * Singleton instance.
+	 */
+	public static AllClassNames INSTANCE = new AllClassNames();
+
+	private AllClassNames() {
+	}
+
+	@Override
+	public int size() {
+		return 1;
+	}
+
+	@Override
+	public void writeTo(StringBuilder builder) {
+		// no-op
+	}
+
+	@Override
+	public List<DexType> asSpecificDexTypes() {
+		return Collections.emptyList();
+	}
+
+	@Override
+	public boolean matches(DexType type) {
+		return true;
+	}
+
+	@Override
+	public void forEachTypeMatcher(Consumer<ProguardTypeMatcher> consumer) {
+		// no-op
+	}
+
+	@Override
+	public TraversalContinuation<?, ?> traverseTypeMatchers(Function<ProguardTypeMatcher, TraversalContinuation<?, ?>> fn) {
+		return TraversalContinuation.doContinue();
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		return o == this;
+	}
+
+	@Override
+	public int hashCode() {
+		// Required implementation, which is dumb as we cannot use the default.
+		return -1289247351;
+	}
+}
